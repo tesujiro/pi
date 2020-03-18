0b3c31bfd12a9a01d7d53afc126393c17edead1d
tesujiro <tesujiro@gmail.com>
2020-03-18 22:22:53 +0900

    add mc in golang

---
+++ mc/pi_MonteCarlo.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	var i, o float64
+	for {
+		rx := rand.Float64()
+		ry := rand.Float64()
+		if rx*rx+ry*ry < 1 {
+			i++
+		} else {
+			o++
+		}
+		fmt.Printf("%v:\t%v\n", i+o, 4*i/(i+o))
+	}
+}
